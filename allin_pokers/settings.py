91329c2b41c0b552ee7f71c583de6ea4f9c3b2a8
nee <nee@hoge.cn>
2017-09-16 01:48:05 +0800

    添加忽略文件

--- allin_pokers/settings.py
+++ allin_pokers/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = [
-    'flat_responsive',
+    # 'flat_responsive',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
